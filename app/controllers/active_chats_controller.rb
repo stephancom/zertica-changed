1837b7400c0dbea96995fe40b9b0ce246492e16e
stephan.com <stephan@stephan.com>
2013-11-29 01:03:48 -0800

    move @chat_partners assignment into controller

--- app/controllers/active_chats_controller.rb
+++ app/controllers/active_chats_controller.rb
@@ -2,7 +2,13 @@ class ActiveChatsController < ApplicationController
   #before_filter :authenticate_admin!
   load_and_authorize_resource 
 
-
+  def index
+    # I cleaned this up for you, eric.  This isn't how I would do it...
+    # but this seemed like the most educational way to interpret how you were doing it.
+    @chat_partners = current_admin.orders.map(&:user).uniq if current_admin
+    @chat_partners = Admin.all if current_user
+    @chat_partners ||= []
+  end
 
   def create
     @active_chat = current_admin.active_chats.new(params[:active_chat])    
