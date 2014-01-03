4afd987ddd36d1327a2ee8661e9aaa5f7c71412c
Eric DiCorrado <blackjacket401@gmail.com>
2014-01-03 00:31:09 -0800

    added :hub in prep for vendors

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140102025151) do
+ActiveRecord::Schema.define(version: 20140103083016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -170,6 +170,7 @@ ActiveRecord::Schema.define(version: 20140102025151) do
     t.string   "state"
     t.boolean  "cad"
     t.boolean  "print"
+    t.boolean  "hub"
   end
 
   create_table "users", force: true do |t|
