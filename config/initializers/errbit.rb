8e1fd50496ffa4fdd1581f54d5fd3b046e5fcf4d
Eric DiCorrado <blackjacket401@gmail.com>
2014-01-07 12:58:44 -0800

    deleted errbit

--- config/initializers/errbit.rb
+++
@@ -1,6 +0,0 @@
-Airbrake.configure do |config|
-  config.api_key = '711f663f4ad595db6362136033a46909'
-  config.host    = 'errors.stephan.com'
-  config.port    = 80
-  config.secure  = config.port == 443
-end
