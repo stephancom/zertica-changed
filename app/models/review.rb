43401bf2505cb77193ab008b721c9e4f00942feb
Eric DiCorrado <blackjacket401@gmail.com>
2014-01-09 00:57:25 -0800

    added ghetto rating system

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,6 @@ class Review < ActiveRecord::Base
 	belongs_to :storefront, dependent: :destroy
 	belongs_to :user
 	validates :title, presence: true
+	validates :description, presence: true
+	validates :rating, presence: true
 end
--- app/models/storefront.rb
+++ app/models/storefront.rb
@@ -3,4 +3,16 @@ class Storefront < ActiveRecord::Base
 	has_many :reviews
 	belongs_to :admin
 	
+	def rating
+      @sum = 0
+	  self.reviews.each do |review|
+		@sum = @sum + review.rating 
+	  end
+	  @rating = (@sum / self.reviews.count.to_f)
+	end
+
+
+
+
+
 end
