7919cb51e1b5ae366464b0b90811d57f85ed577f
stephan.com <stephan@stephan.com>
2014-01-18 01:13:19 -0800

    only force SSL in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 	# gotta force ssl
-	force_ssl
+	force_ssl if Rails.env.production?
 	
 	def current_ability
 	  @current_ability ||= Ability.new(current_account)	
