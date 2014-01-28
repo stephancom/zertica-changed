31bb667f50c7ed2c3f355b57af640ed023476bb1
Eric DiCorrado <blackjacket401@gmail.com>
2014-01-28 00:00:06 -0800

    missing link for product files?

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,5 +5,7 @@ class Product < ActiveRecord::Base
 	validates :admin_id, presence: true
 	belongs_to :admin, dependent: :destroy
 	has_many :file_objects, dependent: :destroy
+	accepts_nested_attributes_for :file_objects, reject_if: proc { |attributes| attributes[:url].blank? }
+
 
 end
