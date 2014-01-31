c7578c4ec88086e0d82e77f6a9ec195915faac6d
Eric DiCorrado <blackjacket401@gmail.com>
2014-01-30 23:08:28 -0800

    --

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,6 +41,7 @@ ZerticaConnect::Application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
