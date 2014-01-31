8eb06cdc7d7cea02bc69bedd03d118fbf0508064
Eric DiCorrado <blackjacket401@gmail.com>
2014-01-31 15:47:41 -0800

    hopefully last syntax change

--- app/assets/javascripts/payment_calc.js
+++ app/assets/javascripts/payment_calc.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-$('input["#subtotal"]').blur(function() {
-    var firstNumber = parseInt($('input["#subtotal"]').val());
+$("#subtotal").blur(function() {
+    var firstNumber = parseInt($("#subtotal").val());
     $('input[name="price"]').val(Math.round(firstNumber*1.18));
 });
 });
