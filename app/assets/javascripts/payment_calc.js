8baf035dc169f959cb244a5ddf143c4e86e28382
Eric DiCorrado <blackjacket401@gmail.com>
2014-01-31 15:36:35 -0800

    does this work?

--- app/assets/javascripts/payment_calc.js
+++ app/assets/javascripts/payment_calc.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-$('input[name="subtotal"]').blur(function() {
-    var firstNumber = parseInt($('input[name="subtotal"]').val());
+$('input[id="#subtotal"]').blur(function() {
+    var firstNumber = parseInt($('input[id="#subtotal"]').val());
     $('input[name="price"]').val(Math.round(firstNumber*1.18));
 });
 });
