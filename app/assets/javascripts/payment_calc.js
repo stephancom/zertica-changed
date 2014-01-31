951f069159e6a073a0f1c8c9f556d1da2d4172b3
Eric DiCorrado <blackjacket401@gmail.com>
2014-01-31 15:12:29 -0800

    syntax fix

--- app/assets/javascripts/payment_calc.js
+++ app/assets/javascripts/payment_calc.js
@@ -1,6 +1,6 @@
-$(document).ready() {
+$(document).ready( {
 $('input[name="subtotal"]').blur(function() {
     var firstNumber = parseInt($('input[name="subtotal"]').val());
     $('input[name="price"]').val(Math.round(firstNumber*1.18));
 });
-};
+});
