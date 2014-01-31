f7dfc267f20e5f07240892ed3fde017a3a02efda
Eric DiCorrado <blackjacket401@gmail.com>
2014-01-31 14:59:25 -0800

    try payment calc again

---
+++ app/assets/javascripts/payment_calc.js
@@ -0,0 +1,4 @@
+$('input[name="subtotal"]').blur(function() {
+    var firstNumber = parseInt($('input[name="subtotal"]').val());
+    $('input[name="price"]').val(Math.round(firstNumber*1.18));
+});
