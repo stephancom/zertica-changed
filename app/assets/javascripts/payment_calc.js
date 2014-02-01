4cc9a2bd8db1b34f29f05ae90f962277ecee3f22
Eric DiCorrado <blackjacket401@gmail.com>
2014-01-31 20:15:07 -0800

    experiment push

--- app/assets/javascripts/payment_calc.js
+++ app/assets/javascripts/payment_calc.js
@@ -1,17 +1,20 @@
 $(document).ready(function() {
       
     $("#rate").blur(function() {
-        if($("#rate_type :selected").text() == "fixed"){
+        //if($("#rate_type :selected").text() == "fixed"){
             var firstNumber = parseInt($("#rate").val());
             $("#price").val(Math.round(firstNumber*1.18));
             $("#subtotal").val(firstNumber);
-        }
+        //}
+        /**
         else {
             var firstNumber = parseInt($("#rate").val());
             var secNumber = parseInt($("#hours").val());
             $("#price").val(Math.round(firstNumber*secNumber*1.18));
             $("#subtotal").val(firstNumber*secNumber);
         }
+
+        **/
         });
     
     $("#hours").blur(function() {
