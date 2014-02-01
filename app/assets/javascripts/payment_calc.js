6f82ca1b4aa35b9a748e3265279f19a717c9ede9
Eric DiCorrado <blackjacket401@gmail.com>
2014-01-31 19:49:31 -0800

    dah fuck?

--- app/assets/javascripts/payment_calc.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-$("#subtotal").blur(function() {
-    var firstNumber = parseInt($("#subtotal").val());
-    $('input[name="price"]').val(Math.round(firstNumber*1.18));
-});
-});
