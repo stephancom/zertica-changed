cd1e97f243683349e0bd5c2863faf69040344459
Eric DiCorrado <blackjacket401@gmail.com>
2014-02-01 19:23:49 -0800

    updated file object model let's see if this breaks it

--- app/models/file_object.rb
+++ app/models/file_object.rb
@@ -1,6 +1,6 @@
 class FileObject < ActiveRecord::Base
-	belongs_to :user
-	has_and_belongs_to_many :orders
+	#belongs_to :user
+	belongs_to :order
 	belongs_to :product
     has_and_belongs_to_many :shipped_orders, class_name: 'Order', join_table: 'orders_shippable_files'
     alias_attribute :name, :filename
